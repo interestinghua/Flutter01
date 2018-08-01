a5721092dd8aaf6da56dc2143ac1665e2b5e23ca
MarsHua <interestinghua@126.com>
2018-08-01 22:03:03 +0800

    commit

--- lib/main2.dart
+++ lib/main2.dart
@@ -1,4 +1,23 @@
 import 'package:flutter/material.dart';
+//import 'package:english_words/english_words.dart';
 
+void main() => runApp(new MyApp());
 
+class MyApp extends StatelessWidget {
 
+  @override
+  Widget build(BuildContext context) {
+//    final wordPair = WordPair.random();
+    return new MaterialApp(
+      title: 'Welcome to flutter2',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter1'),
+        ),
+        body: new Center(
+          child: Text('hello world'),
+        ),
+      ),
+    );
+  }
+}
